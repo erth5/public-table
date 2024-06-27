cd2ed882017c9ec56ad897dc9ef2fc7991b4642b
rthem <rthem@gmx.de>
2024-06-27 18:39:37 +0200

    gen contact

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,9 +12,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //Model::shouldBeStrict();
-        $this->configureRateLimiting();
-
         if ($this->app->environment('production') || $this->app->environment('staging')) {
             URL::forceScheme('https');
         }
@@ -28,18 +23,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-    }
-
-    /**
-     * Configure the rate limiters for the application.
-     */
-    protected function configureRateLimiting(): void
-    {
-        RateLimiter::for('global', function () {
-            return Limit::perMinute(200);
-        });
-        RateLimiter::for('downloads', function (Request $request) {
-            return Limit::perMinute(10)->by($request->user()->id);
-        });
     }
 }
--- resources/views/sites/contact.blade.php
+++ resources/views/sites/contact.blade.php
@@ -5,7 +5,7 @@
 
     <h2 class="center">
         <a
-        href="mailto:{{env('Author_Mail')}}?subject="Public-Laravel_Page" "> Contact: Rthem@gmx.de </a>
+        href="mailto:{{env('Author_Mail')}}?subject="Public-Laravel_Page" "> Contact: {{env('Author_Mail')}} </a>
     </h2>
 @endsection
 
